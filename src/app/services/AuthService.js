eae7f666a012118e385ad1065d5051ce45ca936a
felipe1181 <felipe1181jua@gmail.com>
2020-04-27 15:03:18 -0300

    implement function store in AuthService.js

--- src/app/services/AuthService.js
+++ src/app/services/AuthService.js
@@ -1,2 +1,28 @@
 const connection = require('../models')
-const Response = require('../../constants/response')
+const utilCheckPassword = require('../../utils/CheckPassword')
+const utilGenerateToken = require('../../utils/GenerateToken')
+const HTTP = require('../../constants/http')
+const Response = require('../../constants/ResponseAuth')
+
+const { User } = connection.models
+class AuthService {
+  constructor (user) {
+    this.user = user
+  }
+
+  async store (body) {
+    const { email, password } = body
+    const user = await this.user.findOne({ where: { email } })
+
+    if (!user) return Response.incorretLogin
+    if (!(await utilCheckPassword(password, user.password))) return Response.incorretLogin
+
+    return {
+      user,
+      status: HTTP.OK,
+      token: utilGenerateToken(user.password)
+    }
+  }
+}
+
+module.exports = new AuthService(User)
