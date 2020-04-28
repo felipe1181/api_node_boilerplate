980e3e0df9515521c202c6abb40ad4cc2ee749e5
felipe1181 <felipe1181jua@gmail.com>
2020-04-28 15:13:10 -0300

    auth fix reload token

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,13 +2,14 @@ const HTTP = require('../constants/http')
 const ResponseAuth = require('../constants/ResponseAuth')
 const jwt = require('jsonwebtoken')
 const config = require('../config/env')
-const GenerateToken = require('../utils/GenerateToken')
 
 module.exports = (req, res, next) => {
   try {
     const authHeader = req.headers.authorization
 
     if (!authHeader) return errorToken(res)
+
+    if (!authHeader.startsWith('Bearer')) return errorToken(res)
 
     const authParts = authHeader.split(' ')
     if (!authParts.length === 2) return errorToken(res)
@@ -17,24 +18,14 @@ module.exports = (req, res, next) => {
     if (!/^Bearer/.test(scheme)) return errorToken(res)
 
     jwt.verify(token, config.appSecret, (err, decoded) => {
+      if (err) return errorToken(res)
+
       decoded = jwt.decode(token)
-      if (err) {
-        // refesh token
-        if (err.name === 'TokenExpiredError') {
-          try {
-            req.userId = decoded.id
-            req.refeshToken = 'Bearer ' + GenerateToken(req.userId).toString()
-            return next()
-          } catch (err) { return errorToken(res) }
-        }
-        return errorToken(res)
-      }
       req.userId = decoded.id
-      req.refeshToken = authHeader
       return next()
     })
   } catch (err) {
-    if (err.name === !'TokenExpiredError') return errorToken(res)
+    return errorToken(res)
   }
 }
 
